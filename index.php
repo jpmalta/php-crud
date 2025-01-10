fed4e1d3e91eb1fc68f65c326742d7e81df1387b
jpmalta <joaopedro2512jp@gmail.com>
2025-01-10 14:39:59 -0300

    new index and assets

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+require 'conexao.php';
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD de Usuários</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <h1>Bem-vindo ao Sistema CRUD de Usuários</h1>
+    <p>Selecione uma opção abaixo:</p>
+    <ul>
+        <li><a href="listar_usuarios.php">Listar Usuários</a></li>
+        <li><a href="cadastrar_usuario.php">Cadastrar Novo Usuário</a></li>
+    </ul>
+</body>
+</html>
